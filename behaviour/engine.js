b091227c7ca44e32ec1721a2ac7a71271199cdfe
Elwin Schmitz <elwin@fabrique.nl>
2013-07-24 11:51:06 +0200

    syntax-fixes + versimpelde controller (geen) + ExamplePlugin toegevoegd als dummy

--- behaviour/engine.js
+++ behaviour/engine.js
@@ -1,45 +1,31 @@
-/**
-  Example.nl - engine.js
-*/
 /*jslint browser:true, sloppy:true, vars:true, indent:2 */
 /*global jQuery, $ */
-/*#jslint devel:true, debug:true */
-
-/**
-  Base:
-*/
-var Example = {
-
-  settings: {},
-
-  defaults: {},
-
-  init: function () {
-
-//    console.log('init')
-
-  },
-
-  doSomething: function (event) {
-
-  }
-
-};
+/* jslint devel:true, debug:true */
 
 
-/**
-  Document Ready:
-*/
+
+
+
+
+
+/* =============================================================================
+    Doc Ready:
+============================================================================= */
 $(document).ready(function () {
 
 
 
-/**
-  Skip2 links move focus to target:
-*/
+  /**
+      Skip2 links move focus to target:
+  */
   $('a[href^="#"]').on('click', function () {
     $($(this).attr('href')).focus();
   });
 
 
-}); // end document.ready
+
+  $(document).ExamplePlugin();
+
+
+
+}); // end doc.ready
